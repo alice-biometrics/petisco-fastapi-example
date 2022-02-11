3c26f3ac7dc1a42c7b893840d00337a504083c68
acostapazo <acosta@alicebiometrics.com>
2022-02-11 16:32:04 +0100

    ENH: Add end2end local testing (also added to ci workflow)

--- tests/end2end/client.py
+++ tests/end2end/client.py
@@ -7,5 +7,21 @@ class Client:
         self.host = host
 
     def healthcheck(self) -> Response:
-        response = requests.get(url=self.host + "/healthcheck")
+        response = requests.get(url=f"{self.host}/healthcheck")
+        return response
+
+    def post(self, json: dict) -> Response:
+        response = requests.post(url=f"{self.host}/task", json=json)
+        return response
+
+    def get(self, task_id: str) -> Response:
+        response = requests.get(url=f"{self.host}/task/{task_id}")
+        return response
+
+    def update(self, json: dict) -> Response:
+        response = requests.patch(url=f"{self.host}/task", json=json)
+        return response
+
+    def delete(self, task_id: str) -> Response:
+        response = requests.delete(url=f"{self.host}/task/{task_id}")
         return response
